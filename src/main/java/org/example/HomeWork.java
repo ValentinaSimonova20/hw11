113facac95d39f3671bcba7529db6bcd8cd6b901
valentina <simonova.valentina19@gmail.com>
2024-11-23 00:45:02 +0300

    первое приближение решения

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -3,7 +3,9 @@ package org.example;
 
 import lombok.SneakyThrows;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 
 public class HomeWork {
@@ -14,11 +16,24 @@ public class HomeWork {
      */
     @SneakyThrows
     public void stepDanceValue(InputStream in, OutputStream out) {
-        out.write('3');
-        out.write('\n');
-        out.flush();
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String[] firstLine = br.readLine().split(" ");
+        AVLTree tree = new AVLTree();
+        int n = Integer.parseInt(firstLine[0]);
+        int q = Integer.parseInt(firstLine[1]);
+
+        for (int i = 1; i < n + 1; i++) {
+            tree.add(i);
+        }
+        for (int i = 0; i < q; i++) {
+            int x = Integer.parseInt(br.readLine());
+            tree.update(x);
+            out.write(String.valueOf(tree.getMaxLength()).getBytes());
+            if (i < q - 1) {
+                out.write("\n".getBytes());
+            }
+        }
+
+
     }
-
-
-
 }
